c882b9c161ab073df9c5400f2d90ce8f03eb5689
santosh <santosh.ece06@gmail.com>
2019-05-29 13:00:47 +0530

    day 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Post from 'src/app/post.modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'KodeBook';
+    public title = 'KodeBook';
+
+    public post: Post;
+    public postList: Post[];
+
+    public constructor() {
+      this.post = new Post();
+
+      // SAMPLE IMAGE POST
+      const samplePostImage = new Post();
+      samplePostImage.postType = 'IMAGE';
+      samplePostImage.postValue = 'assets/hbd.jpg';
+
+      this.postList = [samplePostImage];
+    }
+
+    /** Post Text */
+    public postText() {
+      // PUSH METHOD
+      // this.postList.push(this.post);
+
+      // SPLICE - Using this we can add element to any position.
+      this.postList.splice(0, 0, this.post);
+
+      // RE INITIALZE
+      this.post = new Post();
+    }
+
+    /** DELETE POST */
+    public deletePost(itemIndex) {
+       this.postList.splice(itemIndex, 1);
+    }
+
+
+    /** Update Like */
+    public likeCount(item: Post) {
+      item.likeCount += 1;
+    }
+
+    /** Update Subscribe */
+    public subscribeCount(item: Post) {
+      item.subscribeCount += 1;
+    }
+
+    /** Add Comment */
+    public addComment(item: Post) {
+      item.commentList.push('thank you');
+    }
 }
---
+++ src/app/post.modal.ts
@@ -0,0 +1,18 @@
+export default class Post {
+    public postType: string;
+    public postValue: string;
+
+    public likeCount: number;
+    public subscribeCount: number;
+    public commentList: any[];
+
+    public constructor() {
+        // DEFAULT ASSUMPTION
+        this.postType = 'TEXT';
+        this.postValue = '';
+
+        this.likeCount = 0;
+        this.subscribeCount = 0;
+        this.commentList = [];
+    }
+}
